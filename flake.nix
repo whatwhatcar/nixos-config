f3c3fc75a84886a2a32660e5016617db5a4be358
whatwhatcar <184994136+whatwhatcar@users.noreply.github.com>
2026-06-02 17:53:25 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,10 +6,15 @@
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
-     import-tree = {
+    import-tree = {
        url = "github:denful/import-tree";
-     };
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; }
    (inputs.import-tree ./modules);
 }
